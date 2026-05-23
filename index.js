86aa971abf4fd7b29edf5cdbcfb1d15a209b4fc5
Showrav <showravc85@gmail.com>
2026-05-23 20:32:48 +0600

    Server-Side Rendering

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express =require("express");
 const users=require("./MOCK_DATA.json");
 const app=express();
 const PORT=8000;
-app.listen(PORT,()=> console.log('Server Started at port :${PORT}'))
-app.get("/user",(req,res)=>{
+app.listen(PORT,()=> console.log(`Server Started at port :${PORT}`))
+app.get("/api/user",(req,res)=>{
     return res.json(users);
+});
+app.get('/users', (req, res) => {
+    const html = `
+    <ul>
+        ${users.map((user)=>`<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+    res.send(html);
 });
