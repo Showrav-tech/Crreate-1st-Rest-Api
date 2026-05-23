6106c77aafddfcb9d4ed509430659e65d7ca9409
Showrav <showravc85@gmail.com>
2026-05-23 19:54:01 +0600

    Use get user

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const users=require("./MOCK_DATA.json");
 const app=express();
 const PORT=8000;
 app.listen(PORT,()=> console.log('Server Started at port :${PORT}'))
+app.get("/user",(req,res)=>{
+    return res.json(users);
+});
