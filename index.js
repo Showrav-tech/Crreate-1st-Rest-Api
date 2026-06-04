85588ca6de7a8bc3e32a34fdaafdd61a62e30bec
Showrav <showravc85@gmail.com>
2026-06-04 23:00:19 +0600

    Import express

--- index.js
+++ index.js
@@ -1,41 +1,7 @@
-const express =require("express");
-const users=require("./MOCK_DATA.json");
+import express from 'express';
+import bodyParser from 'body-parser';
+
 const app=express();
-const PORT=8000;
-app.listen(PORT,()=> console.log(`Server Started at port :${PORT}`))
-app.get("/api/user",(req,res)=>{
-    return res.json(users);
-});
-app.get('/users', (req, res) => {
-    const html = `
-    <ul>
-        ${users.map((user)=>`<li>${user.first_name}</li>`).join("")}
-    </ul>
-    `;
-    res.send(html);
-});
+const PORT=5000;
 
-//REST API
-app.get("/api/user/",(req,res)=>{
-    return res.json(users);
-});
-
-app.route("/api/users/:id").get("/api/users/:id",(req,res)=>{
-const id=Number(req.params.id);
-const user=users.find((user)=>user.id===id);
-return res.json(user);
-}).put((req,res)=>{
-    res.json({status:"Pending"});
-})
-
-app.post('/api/users',(req,res)=>{
-return res.json({status:"Pending"});
-}).delete((res,req)=>{
-    return res.json({status:"Pending"});
-});
-
-app.post("/api/users",(req,res)=>{
-    return res.json({status:"pending"});
-});
-
-app.listen(PORT,()=>console.log(`Server Started at PORT 8000`));
+app.use(bodyParser.json());
