ede9211dc7f6b587c60f4f2b0224b50e26f528f8
Showrav <showravc85@gmail.com>
2026-06-04 23:11:50 +0600

    Start the server

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 const app=express();
-const PORT=5000;
+const PORT=8000;
 
 app.use(bodyParser.json());
+
+app.listen(PORT,()=>console.log(`Server is running on port : http://localhost${PORT}`));
